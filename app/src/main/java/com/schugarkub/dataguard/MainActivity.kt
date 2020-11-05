08dcba41748cf49e4008c2fea442f713ec5536c4
Vsevolod Sharov <vsevolod.sharov@gmail.com>
2020-11-05 23:07:53 +0300

    UPD: Switch between fragments
    Switch between fragments via bottom navigation

--- app/src/main/java/com/schugarkub/dataguard/MainActivity.kt
+++ app/src/main/java/com/schugarkub/dataguard/MainActivity.kt
@@ -11,12 +11,17 @@ import android.os.Bundle
 import android.os.Process
 import android.provider.Settings
 import androidx.core.app.AppOpsManagerCompat
+import androidx.fragment.app.Fragment
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequestBuilder
 import androidx.work.WorkManager
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.schugarkub.dataguard.utils.ACTION_NOTIFICATION_SENT
 import com.schugarkub.dataguard.utils.NotificationSentReceiver
 import com.schugarkub.dataguard.view.applicationslist.ApplicationsListFragment
+import com.schugarkub.dataguard.view.notificationsjournal.NotificationsJournalFragment
+import timber.log.Timber
+import kotlin.reflect.KClass
 
 private const val REQUEST_USAGE_ACCESS = 100
 
@@ -24,11 +29,35 @@ private const val NETWORK_MONITOR_WORKER_NAME = "com.schugarkub.dataguard.Networ
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var bottomNavigation: BottomNavigationView
+
     private val notificationSentBroadcastReceiver = NotificationSentReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initFragment()
+
+        bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation).apply {
+            setOnNavigationItemSelectedListener { item ->
+                when (item.itemId) {
+                    R.id.applications_list_menu_item -> {
+                        updateFragment(ApplicationsListFragment::class.java)
+                        true
+                    }
+                    R.id.notifications_journal_menu_item -> {
+                        updateFragment(NotificationsJournalFragment::class.java)
+                        true
+                    }
+                    R.id.preferences_menu_item -> {
+                        // TODO Show preferences
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }
 
         createThresholdReachedNotificationChannel()
 
@@ -41,15 +70,6 @@ class MainActivity : AppCompatActivity() {
 
         if (checkIfHaveUsageAccess()) {
             runNetworkMonitorWorker()
-        }
-
-        val containerId = R.id.fragment_container
-        var fragment = supportFragmentManager.findFragmentById(containerId)
-        if (fragment == null) {
-            fragment = ApplicationsListFragment()
-            supportFragmentManager.beginTransaction()
-                .add(containerId, fragment)
-                .commit()
         }
     }
 
@@ -65,6 +85,27 @@ class MainActivity : AppCompatActivity() {
             }
         }
         super.onActivityResult(requestCode, resultCode, data)
+    }
+
+    private fun initFragment() {
+        var fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (fragment == null) {
+            fragment = ApplicationsListFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
+    }
+
+    private fun <T : Fragment> updateFragment(fragmentClass: Class<T>) {
+        var fragment = supportFragmentManager.findFragmentById(R.id.fragment_container)
+        if (!fragmentClass.isInstance(fragment)) {
+            fragment = fragmentClass.newInstance()
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit()
+        }
     }
 
     private fun runNetworkMonitorWorker() {
